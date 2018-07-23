3bccb0b2db79958ab5688c6a7f1dda3ea12ca09b
Fea-Sin <1210992614@qq.com>
2018-07-23 17:56:08 +0800

    first commit

--- src/reducers/counter.js
+++ src/reducers/counter.js
@@ -1,7 +1,7 @@
 export default (state = 0, action) => {
   switch (action.type) {
     case 'INCREMENT':
-      return state + 1
+      return state + 10
     case 'DESCREMENT':
       return state - 1
     default:
