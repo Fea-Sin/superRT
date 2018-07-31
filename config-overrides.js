0855e8d9c05c1cea96973e75a263d45620d0c0a0
Fea-Sin <1210992614@qq.com>
2018-07-31 11:19:07 +0800

    change readme add npm section

--- config-overrides.js
+++ config-overrides.js
@@ -1,6 +1,11 @@
 const { injectBabelPlugin } = require('react-app-rewired')
+const rewireCssModules = require('react-app-rewire-css-modules')
 
 module.exports = function override(config, env) {
+  // 引入 ant-design 配置
   config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
+
+  // 引入 css-modules sass 配置
+  config = rewireCssModules(config, env)
   return config
 }
--- src/views/About/About.js
+++ src/views/About/About.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import './About.css'
+import AboutM from './About.module.scss'
 
 const About = () => (
   <div className='About'>
     <div className='About-con'>
-      HELLO ABOUT
+      <div>HELLO ABOUT</div>
+      <div className={ AboutM.test }>
+        CSS MODULES AND SCSS
+      </div>
     </div>
   </div>
 )
